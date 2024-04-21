922385dff5475bc02ca7eab0a3855a9091041f91
Mahdi-Narimani <mahdi.n.a.m.h.n@gmail.com>
2024-04-21 14:24:17 +0330

    completed

--- main.js
+++ main.js
@@ -1,96 +1,96 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const gameBoarde = document.getElementById("game-board");
-  const scoreDisplay = document.getElementById("score");
-  const liveDisplay = document.getElementById("lives");
-  const startGameBtn = document.getElementById("start-btn");
-  const endGameBtn = document.getElementById("end-btn");
-  const timerDisplay = document.getElementById("timer");
+import { getRandomArbitrary } from "./utils";
 
-  let minutes = 0;
-  let seconds = 0;
-  let timerInterval;
-  let speedGame = 10;
-  const boarderHeight = gameBoarde.scrollHeight;
- 
+const gameBoarde = document.getElementById("game-board");
+const scoreDisplay = document.getElementById("score");
+const liveDisplay = document.getElementById("lives");
+const startGameBtn = document.getElementById("start-btn");
+const endGameBtn = document.getElementById("end-btn");
+const timerDisplay = document.getElementById("timer");
 
-  const shapes = [];
+let minutes = 0;
+let seconds = 0;
+let timerInterval;
+let shapeInterval;
+let shapeIntervalTime = 1000;
+let speedGame = 30;
 
-  let score = 0;
-  let lives = 3;
-  liveDisplay.innerText = lives;
+const boarderHeight = gameBoarde.scrollHeight;
 
-  function startTimer() {
-    timerInterval = setInterval(() => {
-      seconds++;
-      speedGame--;
-      if (seconds === 60) {
-        seconds = 0;
-        minutes++;
-      }
-      timerDisplay.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds
-        .toString()
-        .padStart(2, "0")}`;
-    }, 1000);
-  }
+const shapes = [];
 
-  // Start Game
-  function startGame() {
-    startTimer();
-    startGameBtn.style.display = "none";
-    const numShapes = 2;
-    // Math.floor(Math.random() * 20) + 1; // تعداد تصادفی اشیاء
-    createRandomShapes(numShapes); // ایجاد اشیاء تصادفی
-  }
+let score = 0;
+let lives = 3;
+liveDisplay.innerText = lives;
 
-  // End Game
-  function endGame(Interval) {
-    clearInterval(Interval);
-    gameBoarde.insertAdjacentHTML(
-      "afterbegin",
-      `<div id="modal">
+function startGame() {
+  startTimer();
+  startGameBtn.disabled = true;
+  createShape();
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    seconds++;
+    speedGame--;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+    timerDisplay.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  }, 1000);
+}
+
+function endGame() {
+  gameBoarde.insertAdjacentHTML(
+    "afterbegin",
+    `<div id="modal">
           <div id="close">&times;</div>
           <!-- Modal content -->
           <div class="modal-content">
             <div id='timer'>${minutes.toString().padStart(2, "0")}:${seconds
-        .toString()
-        .padStart(2, "0")}</div>
+      .toString()
+      .padStart(2, "0")}</div>
             <p class='score-modal'>score is: ${score}</p>
             <p class='lives-modal'> ${lives} <i class="fa-solid fa-heart heart-icon"></i> lives</p>
           </div>
       </div>`
-    );
+  );
+  // close modal
+  const modal = document.getElementById("modal");
+  document.getElementById("close").addEventListener("click", () => {
+    modal.style.display = "none";
+    startGameBtn.disabled = false;
+    score = 0;
+    lives = 3;
+    liveDisplay.innerText = lives;
+    scoreDisplay.innerText = score;
+    shapeIntervalTime = 1000;
+    speedGame = 30;
+    minutes = 0;
+    seconds = 0;
+    timerDisplay.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  });
+}
 
-    // close modal
-    const modal = document.getElementById("modal");
-    document.getElementById("close").addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-  }
+function createShape() {
+  const shapesType = ["circle", "square", "trapezius", "triangle"];
+  shapeInterval = setInterval(() => {
+    const boardWidth = gameBoarde.clientWidth - 100;
+    const x = getRandomArbitrary(100, boardWidth);
+    const y = -20;
+    const randomShape =
+      shapesType[Math.floor(Math.random() * shapesType.length)];
 
-  // create random shapes
-  function createRandomShapes(numShapes) {
-    for (let i = 0; i < numShapes; i++) {
-      const randomX = Math.random() * 900; // مختصات افقی تصادفی
-      const randomY = Math.random() * -100; // مختصات عمودی تصادفی
-      createRandomShape(randomX, randomY); // ایجاد شکل با مختصات تصادفی
-    }
-  }
-
-  function createRandomShape(x, y) {
-    const shapes = ["circle", "square", "trapezius", "triangle"];
-    const randomIndex = Math.floor(Math.random() * shapes.length); // نوع شکل تصادفی
-    const randomShape = shapes[randomIndex];
-    createShape(randomShape, x, y); // ایجاد شکل با نوع و مختصات تصادفی
-  }
-
-  // Create Shape with type shape
-  function createShape(type, x, y) {
-    let shape = document.createElement("div");
-    shape.classList.add("shape", type);
-    shape.style.left = `${(Math.random() * x).toFixed(0)}px`;
-    shape.style.top = `${(Math.random() * y).toFixed(0)}px`;
-
+    const shape = document.createElement("div");
+    shape.style.left = `${x}px`;
+    shape.style.top = `${y}px`;
+    shape.classList.add("shape", randomShape);
     gameBoarde.appendChild(shape);
+
     shapes.push(shape);
     moveShape(shape);
 
@@ -99,49 +99,49 @@ document.addEventListener("DOMContentLoaded", () => {
       score++;
       scoreDisplay.innerText = score;
     });
-  }
+    // clearInterval(shapeInterval)
+  }, shapeIntervalTime);
+}
 
-  //  Move Shape on Screen
-  function moveShape(shape) {
-    let top = +shape.style.top.split("px").at(0);
+function moveShape(shape) {
+  let top = +shape.style.top.split("px").at(0);
+  const moveInterval = setInterval(() => {
+    if (lives === 0) {
+      clearInterval(shapeInterval);
+      clearInterval(moveInterval);
+      clearInterval(timerInterval);
+      shapes.length = 0;
+      gameBoarde.innerHTML = "";
+      endGame();
+    }
+    ++top;
+    shape.style.top = `${top}px`;
+    decreaseLifeIfOutOfBounds(); // کم کردن یک جان اگر شکل خارج از صفحه است
+  }, speedGame);
+}
 
-    const moveInterval = setInterval(() => {
-      if (lives === 0) {
-        removeShape(shape);
-        endGame(moveInterval);
-      }
-      top++;
-      shape.style.top = `${top}px`;
-      decreaseLifeIfOutOfBounds(shape); // کم کردن یک جان اگر شکل خارج از صفحه است
-    }, speedGame);
-  }
-
-  function decreaseLifeIfOutOfBounds(shape) {
-    if (shapes.length === 0) clearInterval(timerInterval);
-
-    const top = parseFloat(shape.style.top).toFixed(0);
-
+function decreaseLifeIfOutOfBounds() {
+  shapes.map((shape) => {
+    const top = +parseFloat(shape.style.top).toFixed(0);
     if (top >= boarderHeight) {
       removeShape(shape);
       if (lives > 0) {
         --lives;
         liveDisplay.innerText = lives;
       }
-      if (lives === 0 && shapes.length === 0) {
-        return;
-      }
     }
-  }
+  });
+}
 
-  // Remove Shape from screen and shapes array
-  function removeShape(shape) {
-    shape.remove();
-    const index = shapes.indexOf(shape);
-    if (index !== -1) {
-      shapes.splice(index, 1);
-    }
+// Remove Shape from screen and shapes array
+function removeShape(shape) {
+  shape.remove();
+  shape.style.display = "none";
+  const index = shapes.indexOf(shape);
+  if (index !== -1) {
+    shapes.splice(index, 1);
   }
+}
 
-  startGameBtn.addEventListener("click", startGame);
-  endGameBtn.addEventListener("click", endGame);
-});
+startGameBtn.addEventListener("click", startGame);
+// endGameBtn.addEventListener("click", endGame);
